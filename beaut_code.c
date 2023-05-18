9183fc5c01ca90cd77ae3cf1980a98d5a78a236b
Francis <fhrancorey99@gmail.com>
2023-05-18 03:37:32 +0100

    beautiful code that passes betty checks

---
+++ beaut_code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - The entry point of the program
+  * @argc: parameter of type int .
+  * @argv: parameter of type char const *.
+  * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	printf("Hello World!\n");
+	return (0);
+}
