b29b43e6d86cbc8702fbc89541290f639d4cedc5
Francis <fhrancorey99@gmail.com>
2023-05-21 16:33:01 +0100

    feat: custom function strcmp

--- strcmp.c
+++ strcmp.c
@@ -1,12 +1,12 @@
 #include "main.h"
-/**
- * _strcmp - compares two strings
- * @s1: first string.
- * @s2: second string.
- * Return: 0 if s1 and s2 are equals,
- * another number if not.
- */
 
+/**
+  * _strcmp - compares two strings
+  * @s1: first string
+  * @s2: second string
+  * Return: 0 if s1 and s2 are equals,
+  * another number if not.
+  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0, op = 0;
@@ -14,10 +14,11 @@ int _strcmp(char *s1, char *s2)
 	while (op == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		{
 			break;
+		}
 		op = *(s1 + i) - *(s2 + i);
 		i++;
 	}
-
 	return (op);
 }
