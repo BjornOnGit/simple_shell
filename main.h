31afa15e28c866dbc90b3afaf750ad007b12ac4e
Valentine124 <kaluvalentine60@gmail.com>
2023-05-22 21:11:00 -0700

    added error message implementation

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <sys/wait.h>
 #include "str.h"
 
@@ -12,5 +13,10 @@
 void exec_cmd(const char *filename, char **args);
 void shell_interactive(const char *filename);
 ssize_t _getline(char **lineptr, size_t *n, int fd);
+
+void print_err(char *str, ...);
+int _putchar(char c);
+void print_string(char *s);
+void print_unsigned_number(unsigned int n);
 
 #endif
