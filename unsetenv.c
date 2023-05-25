cc092655ba1707bb738a6f0cef9ada403dd5c502
Francis <fhrancorey99@gmail.com>
2023-05-25 04:25:26 +0100

    fix: betty checks

--- unsetenv.c
+++ unsetenv.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _unsetenv - function that removes 
+ * _unsetenv - function that removes
  * an environment variable
  * @name: a string representing the enviroment
  * variable.
@@ -10,31 +10,29 @@
  */
 int _unsetenv(const char *name)
 {
-    if (name == NULL)
-    {
-        return (1);
-    }
-    int existing_index = -1;
-    char **current = __environ;
+	if (name == NULL)
+	{
+		return (1);
+	}
+	int existing_index = -1;
+	char **current = __environ;
 
-    while (*current != NULL)
-    {
-        if (_strcmp(*current, name) == 0 && (*current)[strlen(name)] == '=')
-        {
-            existing_index = current - __environ;
-            break;
-        }
-        current++;
-    }
-
-    if (existing_index >= 0)
-    {
-        free(__environ[existing_index]);
-    }
-
-    for(int i = existing_index; __environ[i] != NULL; i++)
-    {
-        __environ = __environ[i + 1];
-    }
-    return (0);
+	while (*current != NULL)
+	{
+		if (_strcmp(*current, name) == 0 && (*current)[strlen(name)] == ' = ')
+		{
+			existing_index = current - __environ;
+			break;
+		}
+		current++;
+	}
+	if (existing_index >= 0)
+	{
+		free(__environ[existing_index]);
+	}
+	for (int i = existing_index; __environ[i] != NULL; i++)
+	{
+		__environ = __environ[i + 1];
+	}
+	return (0);
 }
