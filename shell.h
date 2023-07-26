c9db2eb7b3a2978f880afa05fae38fdb501cd236
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-07-26 05:26:26 -0700

    creating 22 functions without nothing inside

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_TOKBUFSIZE 128
+#define MAX_COMM_LENG 1024
+
+
+#endif /* SHELL_H */
